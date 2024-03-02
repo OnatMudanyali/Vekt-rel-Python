756cdd89f60a66f83d86f528f9dcc13e1210d186
OnatMudanyali <161920999+OnatMudanyali@users.noreply.github.com>
2024-03-02 12:12:32 +0300

    Add files via upload

---
+++ python03.py
@@ -0,0 +1,8 @@
+## \ kullanımı
+print('''Eren'in annesi "gel" dedi''')
+print("merhaba nssılsın")
+print("merhaba \nasılsın") # \n = newline
+
+print("numara \teşya ")
+print("1\tabak")
+print("2\tkaşık")
