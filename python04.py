0a706b558f4a87eae460d2ad70d46f67774a74d3
OnatMudanyali <161920999+OnatMudanyali@users.noreply.github.com>
2024-03-02 12:27:12 +0300

    Add files via upload

---
+++ python04.py
@@ -0,0 +1,17 @@
+## \ kullanımı
+print("Onat","Mudanyalı")
+print("Onat"+"Mudanyalı")
+print("İstanbul","Ankara")
+print("İstanbul"+"İstanbul")
+print("Onat"*100)
+print("Süper"*5000)
+
+metin = 12
+şeyma = 13
+print(metin+şeyma)
+
+
+
+
+
+
