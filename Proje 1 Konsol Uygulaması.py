f8493dac2dbd1b487327324820674b3025c94f4f
OnatMudanyali <161920999+OnatMudanyali@users.noreply.github.com>
2024-03-02 13:03:16 +0300

    Proje 1 Konsol Uygulaması

---
+++ Proje 1 Konsol Uygulaması.py
@@ -0,0 +1,17 @@
+# 201 ╔ # 205 ═ # 187 ╗ # 186 ║ # 200 ╚ # 188 ╝
+
+print("╔══════════════════════╗")
+print("║  PYTHON ÇALIŞMALARI  ║")
+print("║ 1-Hesap Makinası     ║")
+print("║ 2-Oyunlar            ║")
+print("║ 3-Şekil Çizdirme     ║")
+print("║ 4-Takvim             ║")
+print("║5-Çıkış (e)           ║")
+print("╚══════════════════════╝")
+
+
+
+
+
+
+
