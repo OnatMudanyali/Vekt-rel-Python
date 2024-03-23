f892535e432b019f62b3df653c15910badc97e75
Onat Mudanyalı <161920999+OnatMudanyali@users.noreply.github.com>
2024-03-23 09:25:50 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,8 +1,11 @@
-onat= 12
-print(onat)
-ad= onat
-Yas= 12
-print(Yas)
-print("Demek Yasın",Yas)
-print(f"[ad] Demek Yaşın[Yas]")
-print("[] Demek Yaşın []".format(ad,Yas))
+##a = 5
+##b = 7
+##a = input()
+##b = input()
+a = int(input("1.Sayıyı gir:"))
+b = int(input("2.Sayıyı gir"))
+print("Toplam:",a+b)
+print("Çarpım:",a*b)
+print("İki sayının karesini al:",a*a+b*b)
+print("İlk sayının 10 katını al ve 2.Sayının 5 katını al:",a*10+b*5)
+print("a'yı b'ye böl:",a/b)
